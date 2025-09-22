abe378b783572e0fe2d7c9e1d2b14c27990ffca9
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-22 23:19:14 +0200

    Update README and build_doc script to enhance documentation structure with multipage and singlepage HTML outputs, and improve index.html layout for better navigation.

--- bin/build_doc.sh
+++ bin/build_doc.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 cd doc
+
+# README 2 index.html
+asciidoctor ../README.adoc -o index.html
+
+# multipage documentation
 asciidoctor-multipage -D organizing 1.ORGANIZING.adoc
 asciidoctor-multipage -D testing 2.TESTING.adoc 
+
+# html documentation
+asciidoctor 1.ORGANIZING.adoc
+asciidoctor 2.TESTING.adoc
+
 cd ..
 
